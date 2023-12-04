20520f9227c27ee188d65b41f6d8e3a7f411b6ef
Shivendra Singh <shiven@alfheimr.tech>
2023-12-04 21:21:40 +0530

    Added bundler. Project setup complete

--- src/smart-wallet.ts
+++ src/smart-wallet.ts
@@ -17,15 +17,23 @@ async function createWallet() {
         throw new Error("wallet is already initialized");
     }
 
-    ethersWallet = new Wallet("SHIVEN");
     console.log("Creating Wallet");
+
+    ethersWallet = new Wallet("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
     console.log(ethersWallet.address);
     console.log(ethersWallet.privateKey);
 }
 
-// Get the button element
-const createWalletButton = document.getElementById('createWalletBtn');
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('createWalletBtn');
+    if (button) {
+        button.addEventListener('click', createWallet);
+    }
+});
 
-// Attach click event listener to the button
-if (createWalletButton)
-    createWalletButton.addEventListener('click', createWallet);
+// // Get the button element
+// const createWalletButton = document.getElementById('createWalletBtn');
+
+// // Attach click event listener to the button
+// if (createWalletButton)
+//     createWalletButton.addEventListener('click', createWallet);

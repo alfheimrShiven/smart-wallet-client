496a75bb923971932eec13b2c7e2ba404b2a6ffe
Shivendra Singh <shiven@alfheimr.tech>
2023-12-14 21:38:11 +0530

    creating a basic wallet using Ether.js

--- src/smart-wallet.ts
+++ src/smart-wallet.ts
@@ -1,27 +1,13 @@
-import { Wallet } from "ethers";
+import { ethers, HDNodeWallet, Wallet } from "ethers";
 
-export type WalletData = {
-    address: string;
-    strategy: "mnemonic" | "privateKey" | "encryptedJson";
-    data: string;
-    isEncrypted: boolean;
-};
-let ethersWallet: Wallet;
+function createWallet() {
+    let newWallet: HDNodeWallet = ethers.Wallet.createRandom();
+    let walletAddress: string = newWallet.address;
+    let walletPublicKey: string = newWallet.publicKey;
+    let walletPrivateKey: string = newWallet.privateKey;
+    let mnemonic: ethers.Mnemonic | null = newWallet.mnemonic;
 
-/**
-* creates a new random wallet
-* @returns the address of the newly created wallet
-*/
-async function createWallet() {
-    if (ethersWallet) {
-        throw new Error("wallet is already initialized");
-    }
-
-    console.log("Creating Wallet");
-
-    ethersWallet = new Wallet("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
-    console.log(ethersWallet.address);
-    console.log(ethersWallet.privateKey);
+    console.log(`Wallet created ${walletAddress}, \n with public key: ${walletPublicKey}, \n private key: ${walletPrivateKey}`);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -31,9 +17,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// // Get the button element
-// const createWalletButton = document.getElementById('createWalletBtn');
-
-// // Attach click event listener to the button
-// if (createWalletButton)
-//     createWalletButton.addEventListener('click', createWallet);

5c746cd54b2a8bebc7a141f2ae12934b4261a45c
Shivendra Singh <shiven@alfheimr.tech>
2023-12-19 19:00:15 +0530

    replaced web3.js with Ethers.js for contract interactions

--- src/smart-wallet.ts
+++ src/smart-wallet.ts
@@ -1,7 +1,6 @@
-import { ethers, HDNodeWallet, Wallet } from "ethers";
-import AccountRecoveryContractABI from "./account-recovery-abi.json";
+import { HDNodeWallet, ethers, Contract, Interface } from "ethers";
+import AccountRecoveryContractABIJSON from "./account-recovery-abi.json";
 import * as shamir from 'shamir-secret-sharing';
-var Contract = require('web3-eth-contract');
 
 let walletAddress: string;
 let walletPrivateKey: string;
@@ -10,10 +9,12 @@ let walletPublicKey: string;
 const CONTRACT_ADDRESS: string = "0x2ACDe8bc8567D49CF2Fe54999d4d4A1cd1a9fFEA";
 const RPC_URL: string = "http://localhost:8545";
 
-function getContract(): any {
-    Contract.setProvider(RPC_URL);
-    var contract: any = new Contract(AccountRecoveryContractABI, CONTRACT_ADDRESS);
-    return contract;
+async function getContract() {
+    const provider = new ethers.JsonRpcProvider(RPC_URL);
+    const signer = await provider.getSigner();
+    const AccountRecoveryContractABI = new Interface(AccountRecoveryContractABIJSON.abi);
+
+    return new Contract(CONTRACT_ADDRESS, AccountRecoveryContractABI, signer);
 }
 
 function createWallet() {
@@ -21,7 +22,6 @@ function createWallet() {
     walletAddress = newWallet.address;
     walletPublicKey = newWallet.publicKey;
     walletPrivateKey = newWallet.privateKey;
-    let mnemonic: ethers.Mnemonic | null = newWallet.mnemonic;
 
     console.log(`Wallet created ${walletAddress}, \n with public key: ${walletPublicKey}, \n private key: ${walletPrivateKey}`);
 }
@@ -31,8 +31,9 @@ async function splitPrivateKeyIntoShards() {
     const [share1, share2, share3] = await shamir.split(privateKey, 3, 2);
 
     const privateKeyShardsArray: Uint8Array[] = [share1, share2, share3];
-    const AccountRecoveryContract = getContract();
-    AccountRecoveryContract.methods.storePrivateKeyShards(privateKeyShardsArray).send();
+    const AccountRecoveryContract = await getContract();
+    const tx = await AccountRecoveryContract.storePrivateKeyShards(privateKeyShardsArray);
+    await tx.wait();
 }
 
 const toUint8Array = (data: string) => new TextEncoder().encode(data);

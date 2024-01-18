239065e800132e033577884a2eb8f7226eabe9af
techyNonso <williamikeji@gmail.com>
2024-01-18 16:51:44 +0100

    contract interaction error

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { ConnectWallet, useConnectionStatus, useWallet, useSmartWallet, embeddedWallet, useDisconnect } from '@/providers/ThirdwebProvider';
+import { ConnectWallet, useConnectionStatus, useWallet, useDisconnect, useContract, useContractWrite, Web3Button } from '@/providers/ThirdwebProvider';
+import abi from "@/abi/AccountFactory.json"
+import { ethers } from 'ethers';
 
 
 export default function index() {
     const [email, setEmail] = useState<string | undefined>(undefined)
+    const [wallet, setWallet] = useState<string | undefined>(undefined)
     const disconnect = useDisconnect()
 
     const connectionStatus = useConnectionStatus();
     const embeddedWalletObj = useWallet("embeddedWallet");
 
-    const { connect, predictAddress } = useSmartWallet(embeddedWallet(), {
-        factoryAddress: `${process.env.NEXT_PUBLIC_ACCOUNT_FACTORY_ADDRESS}`,
-        gasless: true,
-    });
+    const { data: contract } = useContract(`${process.env.NEXT_PUBLIC_ACCOUNT_FACTORY_ADDRESS}`, abi);
+    const { mutateAsync, isLoading, error } = useContractWrite(
+        contract,
+        "createAccount",
+    );
+
+    // const { connect, predictAddress } = useSmartWallet(embeddedWallet(), {
+    //     factoryAddress: `${process.env.NEXT_PUBLIC_ACCOUNT_FACTORY_ADDRESS}`,
+    //     gasless: true,
+    // });
 
     // const onClick = async () => {
     //     await connect({
@@ -27,30 +36,29 @@ export default function index() {
     //     });
     // };
 
+    const createWallet = async () => {
+        // @ts-ignore
+        const data = mutateAsync({ args: [wallet, ethers.utils.formatBytes32String(email)] })
+        console.log(data)
+    }
+
 
     const getUserData = async () => {
         const email = await embeddedWalletObj?.getEmail();
         const wallet = await embeddedWalletObj?.getAddress()
-
         setEmail(email)
+        setWallet(wallet)
 
         if (wallet) {
-            const address = await predictAddress({
-                personalWalletAddress: wallet,
-            });
             console.log(" Normal Wallet address", wallet)
-            console.log("Predicted Smart Wallet address", address);
         }
     }
-
-
     useEffect(() => {
         getUserData()
     })
 
     return (
         <form className='w-[80%] max-w-[400px] h-[80%]  flex flex-col justify-between '>
-            {/* <button type='button' onClick={onClick} className='border'>Click Test</button> */}
             <button type='button' onClick={() => disconnect()} className='border'>Disconnect</button>
             {(connectionStatus == "connecting" || connectionStatus == "unknown") && <div className='h-[50%] flex flex-col justify-center gap-5'>
                 <h3 className='font-semibold text'>Welcome  &#128075;</h3>
@@ -73,7 +81,8 @@ export default function index() {
                     <input className='w-full h-full outline-none focus:ring-0 focus:border-none border-none bg-transparent px-3'
                         placeholder='OTP' />
                 </div> */}
-                <button type='button' className='bg-primary_3 rounded-full px-3  py-3 font-semibold text-sm '>Create Wallet</button>
+                <button type='button' onClick={createWallet} className='bg-primary_3 rounded-full px-3  py-3 font-semibold text-sm '>Create Wallet</button>
+
                 <button type='button' className='bg-secondary_2 rounded-full px-3  py-3 font-semibold text-sm '>Start Wallet Recovery</button>
             </div>}
             <div className=''>
--- src/providers/ThirdwebProvider.tsx
+++ src/providers/ThirdwebProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export { ThirdwebProvider, embeddedWallet, ConnectWallet, useWallet, useConnectionStatus, smartWallet, useSmartWallet, useDisconnect } from "@thirdweb-dev/react";
+export { ThirdwebProvider, embeddedWallet, ConnectWallet, useWallet, useConnectionStatus, useSmartWallet, useDisconnect, useContractWrite, useContract, Web3Button } from "@thirdweb-dev/react";
 export { SmartWallet } from "@thirdweb-dev/wallets";
 
 export { AreonNetworkTestnet, Goerli, AreonNetwork, Mumbai } from "@thirdweb-dev/chains";
